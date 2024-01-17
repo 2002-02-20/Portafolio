44439359d6e974359145bfe3e4ea235acaaa2738
Derek <adhoc607@gmail.com>
2024-01-17 13:07:34 -0500

    Tech Stack

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 /* import viteLogo from '/vite.svg' */
 import './App.css'
 import Navbar from './components/Navbar'
-import Presentacion from './components/Navbar/Presentacion'
+import Presentacion from './components/Presentacion'
+import TechStack from './components/TechStack'
 function App() {
 
   return (
     <>
-     <Navbar/>
-      <Presentacion/> 
+    <Navbar/>
+    <Presentacion/>
 
+    <TechStack/>
     </>
   )
 }
